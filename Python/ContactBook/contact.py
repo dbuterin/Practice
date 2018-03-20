9afef6ee1bbf7a14a3eb10da29b5f1af211a64d1
Davor Buterin <buterindavor@gmail.com>
2018-03-20 07:47:02 +0100

    no message

--- Python/ContactBook/contact.py
+++
@@ -1,6 +0,0 @@
-class Contact(object):
-    def setData(self):
-        self.first_name = raw_input("Unesi ime: ")
-        self.last_name = raw_input("Unesi prezime: ")
-    def fullName(self):
-        return self.first_name + " " +self.last_name
